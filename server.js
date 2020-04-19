2beeee77b453b6a94007a57723d889fc51c01138
hugh <bup@bup.com>
2020-04-18 20:25:57 -0500

    trying again

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-let api_routes = require('./routes/api.js')
 let path = require('path')
+
+let api_routes = require('./routes/api.js')
 
 let app = express()
 
